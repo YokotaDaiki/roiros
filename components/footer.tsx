5a3c38e82203f34a758e7063c5e04cb1a4eb4827
YokotaDaiki <yokota@asnica.co.jp>
2022-05-01 01:59:01 +0900

    フッター作成

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,9 @@
-import { useState } from 'react';
 export default function Footer() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div>
-      <p>{count}</p>
-      <div>
-        <button onClick={() => setCount(count + 1)}>Count+</button>
-      </div>
-      <p>Footerコンポーネント</p>
-    </div>
+    <footer className="p-4 bg-gray-800 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 text-center">
+      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        © 2022 <a href="#" className="hover:underline">ROIROS</a>. All Rights Reserved.
+      </span>
+    </footer>
   );
 }
