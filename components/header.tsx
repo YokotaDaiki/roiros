5c55773d27ee07493699820fb1a7489231f56b02
YokotaDaiki <yokota@asnica.co.jp>
2022-05-01 15:35:44 +0900

    ヘッダーのリンクの部分を修正

--- components/header.tsx
+++ components/header.tsx
@@ -139,20 +139,6 @@ export default function Header() {
               </div>
             </div>
             <div className="py-6 px-5 space-y-6">
-              <div>
-                <a
-                  href="#"
-                  className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-                >
-                  Sign up
-                </a>
-                <p className="mt-6 text-center text-base font-medium text-gray-500">
-                  Existing customer?{' '}
-                  <a href="#" className="text-indigo-600 hover:text-indigo-500">
-                    Sign in
-                  </a>
-                </p>
-              </div>
             </div>
           </div>
         </Popover.Panel>
